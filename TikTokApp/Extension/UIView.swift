3fa2ab646b31c48d193d7bcee0aa9b0dd8ac6ca2
Yennifer Hurtado <yennifer.hurtadoarce@gmail.com>
2023-08-28 16:10:44 -0500

    Welcome View UI Storyboard

--- TikTokApp/Extension/UIView.swift
+++ TikTokApp/Extension/UIView.swift
@@ -5,4 +5,12 @@
 //  Created by Yennifer Hurtado Arce on 28/08/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    public func addCornerRadius(_ radius: CGFloat = 4) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = true
+    }
+}
---
+++ TikTokApp/Flows/Welcome/WelcomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeVC.swift
+//  TikTokApp
+//
+//  Created by Yennifer Hurtado Arce on 28/08/23.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBOutlet weak var signUpEmailButton: UIButton!
+    @IBOutlet weak var loginFacebookButton: UIButton!
+    @IBOutlet weak var loginGoogleButton: UIButton!
+    @IBOutlet weak var loginEmailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    func setupUI() {
+        signUpEmailButton.addCornerRadius(18)
+        loginFacebookButton.addCornerRadius(18)
+        loginGoogleButton.addCornerRadius(18)
+        loginEmailButton.addCornerRadius(18)
+    }
+
+}
+
